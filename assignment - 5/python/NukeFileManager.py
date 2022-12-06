bc15b91c9a1356d9ea521a7cf596f8e81ba600c9
Jonathan Lim <jjl388@drexel.edu>
2022-12-05 23:56:04 -0500

    Code Comments

--- assignment - 5/python/NukeFileManager.py
+++ assignment - 5/python/NukeFileManager.py
@@ -2,3 +2,25 @@
 
 import nuke
 import 'YOUR PY SCRIPT NAME'
+
+#And then your code here
+
+
+
+
+
+#Allow the user to select the folder containing elements
+#Allow the user to select what elements they desire, if any specific
+#Allow the user to select what version they desire
+
+
+#Import JSON file
+
+
+#Show the elements in the folder
+    #for each element sequence
+        # ex) thorTest (1-123), prettyTrees (34-128)
+#Allow the user to select which elements to import in
+#Create a read node for each sequence
+
+#Create write output, destination and naming convention, by reading the JSON file
